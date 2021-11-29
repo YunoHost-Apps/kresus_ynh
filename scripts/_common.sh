1b18126a28f14c522f010cdaecf5795ee093051b
ericgaspar <junk.eg@free.fr>
2021-11-29 08:09:45 +0100

    4.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -21,31 +21,6 @@ nodejs_version=14
 # FUTURE OFFICIAL HELPERS
 #=================================================
 
-# Check the architecture
-#
-# example: architecture=$(ynh_detect_arch)
-#
-# usage: ynh_detect_arch
-#
-# Requires YunoHost version 2.2.4 or higher.
-
-ynh_detect_arch(){
-	local architecture
-	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
-		architecture="arm64"
-	elif [ -n "$(uname -m | grep 64)" ]; then
-		architecture="x86-64"
-	elif [ -n "$(uname -m | grep 86)" ]; then
-		architecture="i386"
-	elif [ -n "$(uname -m | grep arm)" ]; then
-		architecture="arm"
-	else
-		architecture="unknown"
-	fi
-	echo $architecture
-}
-
-#=================================================
 
 # Send an email to inform the administrator
 #
